e47342c934e0010372d627707140565295a0103d
APrajapat10 <coolprajapat@gmail.com>
2024-09-13 18:05:30 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import OTPVerificationScreen from './screens/OTPVerificationScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="OTPVerification" component={OTPVerificationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
