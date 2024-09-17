ead7c6df8c3338bba0362fc13a25864514875a6c
APrajapat10 <coolprajapat@gmail.com>
2024-09-17 17:29:52 +0530

    Temp turned off pass validaiton code

--- src/validation/loginValidation.js
+++ src/validation/loginValidation.js
@@ -4,9 +4,9 @@ export const loginValidationSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email address').required('Email is required'),
   password: Yup.string()
     .min(6, 'Password must be at least 6 characters')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/[0-9]/, 'Password must contain at least one number')
-    .matches(/[\W_]/, 'Password must contain at least one special character')
+    // .matches(/[a-z]/, 'Password must contain at least one lowercase letter') TEMP TURNED OFF FOR EASIER DEV/TEST
+    // .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    // .matches(/[0-9]/, 'Password must contain at least one number')
+    // .matches(/[\W_]/, 'Password must contain at least one special character')
     .required('Password is required'),
 });
