9f92a0fc045160c80833653def8e361fd4123a1b
Shriya Kapil <shriyakapil@Shriyas-MacBook-Air.local>
2024-10-14 14:36:58 +0530

    Revert "Merge pull request #6 from Project-Dark-Horse/feature/home"
    This reverts commit c50cc9c0055ec3838d08d84ba6604055f1e0738d, reversing
    changes made to 7d1d61c90ce03aae90cfd1340026585d6ebfc277.

--- src/components/MainPage/UpcomingTasks.tsx
+++ src/components/MainPage/UpcomingTasks.tsx
@@ -12,7 +12,7 @@ const UpcomingTasks = [
     title: 'Team Meeting',
     description: 'Group discussion for the new product',
     time: '10 AM',
-    backgroundColor: '#5560C4',
+    backgroundColor: '#4956C7',
     iconName: 'people', 
   },
   {
@@ -20,7 +20,7 @@ const UpcomingTasks = [
     title: 'Design Review',
     description: 'Review of the new design prototype',
     time: '2 PM',
-    backgroundColor: '#36AAB9',
+    backgroundColor: '#3C8FA9',
     iconName: 'chatbubble', 
   },
   {
@@ -28,7 +28,7 @@ const UpcomingTasks = [
     title: 'Code Review',
     description: 'Review of the recent code changes',
     time: '4 PM',
-    backgroundColor: '#499FCE',
+    backgroundColor: '#3D83AA',
     iconName: 'notifications', 
   },
 ];
