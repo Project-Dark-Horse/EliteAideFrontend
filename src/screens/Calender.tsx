f18d13c7b878eda75d04aec65da8320e5bd1799c
Priyanka Deshmukh <priyankadeshmukh.h@gmail.com>
2024-10-21 12:17:16 +0530

    bugfix

--- src/screens/Calender.tsx
+++ src/screens/Calender.tsx
@@ -2,6 +2,7 @@ import { View } from "react-native";
 import Header from "../components/calendar/Header"
 import tw from 'twrnc';
 import DaySelector from "../components/calendar/DateSelector";
+import React from "react";
 
 const Calender = () => {
   return (
